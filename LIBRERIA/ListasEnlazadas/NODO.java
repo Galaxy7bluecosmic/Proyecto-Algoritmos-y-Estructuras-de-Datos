8eee0f716c39ea35d093f85406d60a518f2d66ab
Galaxy7bluecosmic <Vizcardocanovictorpaul@gmail.com>
2025-11-13 18:26:27 -0500

    NODO DE LA LISTA ENLAZADA DOBLE

---
+++ LIBRERIA/ListasEnlazadas/NODO.java
@@ -0,0 +1,15 @@
+package pf_ectructuradedatos;
+
+/**
+ *
+ * @author ttito
+ */
+public class Nodo {
+    int dato;
+    Nodo siguiente;
+    
+    public Nodo(int dato){
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
