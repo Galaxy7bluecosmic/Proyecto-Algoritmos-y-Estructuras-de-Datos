8b4d5cf54d570faa3edd2a0398741c248f9eb5bb
Galaxy7bluecosmic <Vizcardocanovictorpaul@gmail.com>
2025-11-18 15:55:01 -0500

    Create NODO.java

---
+++ LIBRERIA/COLA/NODO.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Victor
+ */
+public class NODO {
+    Object dato;
+    NODO siguiente;
+    
+    public NODO(Object dato){
+    this.dato=dato;
+    this.siguiente=null;
+    }
+}
