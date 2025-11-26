dad9a0db7ee109a115a58d07615b5adb7b86570a
Galaxy7bluecosmic <Vizcardocanovictorpaul@gmail.com>
2025-11-26 18:16:02 -0500

    Update NODO.java

--- LIBRERIA/PILA/NODO.java
+++ LIBRERIA/PILA/NODO.java
@@ -1,13 +1,21 @@
-/**
- *
- * @author Victor
- */
-public class NODO {
-    Object dato;
-    NODO siguiente;
-    
-    public NODO(Object dato){
-    this.dato=dato;
-    this.siguiente=null;
+
+package Libreria;
+
+public class Nodo <T> {
+ 
+  
+   Nodo<T> hojaDerecha;
+   Nodo<T> hojaIzquierda;
+  
+    Nodo<T> siguiente;
+    Nodo<T> anterior;
+    T dato;
+
+    public Nodo(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
     }
+    
+    
 }
