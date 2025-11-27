5eabcf21a38ea183dc8d9314d44963ddae5f98b0
Galaxy7bluecosmic <Vizcardocanovictorpaul@gmail.com>
2025-11-26 19:13:37 -0500

    Update and rename que hacer to que hacer.java

---
+++ Servicios/Gestion de Historial de Misiones/que hacer.java
@@ -0,0 +1,77 @@
+MÓDULO 1 — Catálogo de Logros (Lista Enlazada Doble)
+
+Registrar logros
+
+Editar logros
+
+Eliminar logros
+
+Listarlos
+
+Filtrar logros (categoría / dificultad / tipo)
+
+Cargar desde XML
+
+Guardar en XML
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Modelos.PerfilDeJugador;
+import Libreria.ImplementacionCola;
+import Libreria.ImplementacionTADDoblementecircular;
+/**
+ *
+ * @author victor
+ */
+public class Jugador {
+    private int id;
+    private String nickname;
+    private ImplementacionCola Pilamisiones;
+    private ImplementacionTADDoblementecircular LogrosObtenidos;
+    
+    public Jugador(int id,String nickname){
+    this.id=id;
+    this.nickname=nickname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public ImplementacionCola getPilamisiones() {
+        return Pilamisiones;
+    }
+
+    public ImplementacionTADDoblementecircular getLogrosObtenidos() {
+        return LogrosObtenidos;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setPilamisiones(ImplementacionCola Pilamisiones) {
+        this.Pilamisiones = Pilamisiones;
+    }
+
+    public void setLogrosObtenidos(ImplementacionTADDoblementecircular LogrosObtenidos) {
+        this.LogrosObtenidos = LogrosObtenidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "id=" + id + ", nickname=" + nickname + ", Pilamisiones=" + Pilamisiones + ", LogrosObtenidos=" + LogrosObtenidos + '}';
+    }
+    
+    
+}
